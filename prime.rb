6ba76c6f6da99b4936069f6600b2863905fe2486
Vince Andrew Pimentel <contact@vincep.me>
2018-10-16 20:25:07 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,4 @@
 # Add  code here!
 def prime?(number)
-  n = 2
-  if number < 2
-    return false
-  end
 
-  while n < number
-    if number % n == 0
-      n += 1
-      return false
-    end
-  end
-  return true
 end
