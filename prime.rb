4006e126868a6734570dde11fe3429da440645c2
Vince Andrew Pimentel <contact@vincep.me>
2018-10-16 20:13:16 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,24 +1,4 @@
 # Add  code here!
 def prime?(n)
-  if n <= 1
-    return false
-  elsif n <= 3
-    return true
-  elsif n % 2 == 0 || n % 3 == 0
-    return false
-  end
-
-  i = 5
-  w = 2
-
-  while i * i <= n
-    if n % i == 0
-      return false
-    end
-
-    i += w
-    w = 6 - w
-  end
-
-  return true    
+  !((2..number-1).any? {|i| number % i == 0}) && number > 1
 end
