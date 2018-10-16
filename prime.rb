e41840f3e86afb516ef200e1c44da27a2b3217bf
Vince Andrew Pimentel <contact@vincep.me>
2018-10-16 20:32:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(number)
-  if (2..number).any? { |i| number % i == 0 } && !number < 1
+  if (2..number).any? { |i| number % i == 0 } && number > 1
     return false
   end
 end
