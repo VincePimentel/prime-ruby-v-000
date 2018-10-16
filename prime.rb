6bf9e1b31f91056ac911fa25093be1acc84745e2
Vince Andrew Pimentel <contact@vincep.me>
2018-10-16 19:58:08 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,10 @@
 # Add  code here!
 def prime?(number)
-
+  if number <= 1
+    return false
+  elsif number <= 3
+    return true
+  elsif number % 2 == 0 || number % 3 == 0
+    return false
+  end
 end
