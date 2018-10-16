a327a733b0165fa57b47e0a2e27e3f685c4215ba
Vince Andrew Pimentel <contact@vincep.me>
2018-10-16 20:04:39 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,24 @@
 # Add  code here!
-def prime?(number)
-  if number <= 1
+def prime?(n)
+  if n <= 1
     return false
-  elsif number <= 3
+  elsif n <= 3
     return true
-  elsif number % 2 == 0 || number % 3 == 0
+  elsif n % 2 == 0 || n % 3 == 0
     return false
   end
+
+  i = 5
+  w = 2
+
+  while i * i <= n
+    if n % i == 0
+      return false
+    end
+
+    i += w
+    w = 6 - w
+  end
+
+  return true    
 end
